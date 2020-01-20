f110d8d01fa6ab0d10fd2df3eb888dcc71e654fc
Christopher McArthur <prince.chrismc@gmail.com>
2020-01-19 22:08:17 -0500

    prototype for FindPackage install

--- vcpkg/portfile.cmake
+++ vcpkg/portfile.cmake
@@ -15,6 +15,8 @@ vcpkg_configure_cmake(
 # Copy the constexpr header files
 vcpkg_install_cmake()
 
+vcpkg_test_cmake(PACKAGE_NAME jwt-cpp)
+
 # Put the licence file where vcpkg expects it
 file(COPY ${SOURCE_PATH}/LICENSE
      DESTINATION ${CURRENT_PACKAGES_DIR}/share/jwt-cpp)
