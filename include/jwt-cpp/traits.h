c5663f5f2b0465c93d482268d0faf5e7f862169b
Chris Mc <prince.chrismc@gmail.com>
2020-05-16 00:17:26 -0400

    Update traits.h

--- include/jwt-cpp/traits.h
+++ include/jwt-cpp/traits.h
@@ -2,12 +2,22 @@
 
 namespace jwt {
     namespace json {
+        enum class type {
+            null, 
+            boolean, 
+            integer,
+            number, 
+            string, 
+            array, 
+            object, 
+        };
+
         template<typename value, 
-            enum type, 
             typename object, 
             typename array, 
             typename string, 
             typename number, 
+            typename integer,
             typename boolean, 
             /*typename null = nullptr*/>
         struct traits {
@@ -18,6 +28,7 @@ namespace jwt {
             using string = string;
             using number = number;
             using boolean = boolean;
+            using integer = integer;
             // using null = null;
         };
     }
@@ -25,11 +36,24 @@ namespace jwt {
 
 struct picojson_traits : traits<
     picojson::value, 
-    picojson::type, // TBA
     picojson::object, 
     picojson::array, 
     std::string, 
+    double,
     int64_t, 
     bool> {
+    static jwt::json::type get_type(const traits::value& val) {
+        using jwt::json::type;
+
+        if (val.is<picojson::null>()) return type::null;
+	else if (val.is<bool>()) return type::boolean;
+	else if (val.is<int64_t>()) return type::integer;
+	else if (val.is<double>()) return type::number;
+	else if (val.is<std::string>()) return type::string;
+	else if (val.is<picojson::array>()) return type::array;
+	else if (val.is<picojson::object>()) return type::object;
+	else throw std::logic_error("invalid type");
+		
+    }
 };
     
