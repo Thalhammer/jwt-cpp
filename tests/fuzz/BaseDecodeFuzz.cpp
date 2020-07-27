071df33d58e80445c6e8edb66efa3d4fb64f1d0b
Chris Mc <prince.chrismc@gmail.com>
2020-07-26 23:12:53 -0400

    base64 encoding input to avoid "obvious" exceptions
    trying out the EVP_ interface from #89

--- tests/fuzz/BaseDecodeFuzz.cpp
+++ tests/fuzz/BaseDecodeFuzz.cpp
@@ -1,9 +1,15 @@
 #include <jwt-cpp/base.h>
+#include <openssl/evp.h>
 
 extern "C" {
 
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
-  jwt::base::decode<jwt::alphabet::base64>(std::string{(char *)Data, Size});
+  std::vector<uint8_t> encoded(4*((Size+2)/3, 0);
+  if(EVP_EncodeBlock(encoded.data(), Data, Size) != encoded.size())
+    abort(); // Critical error using OpenSSL
+                               
+  jwt::base::decode<jwt::alphabet::base64>(std::string{encoded.begin(), encoded.end()}); // TO DO: Compare against input
   return 0; // Non-zero return values are reserved for future use.
 }
+
 }
