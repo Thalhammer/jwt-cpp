b8ddb84155b0495cbbdf031c302583de0f8507dd
Chris Mc <prince.chrismc@gmail.com>
2020-07-28 22:47:46 -0400

    accepting exceptions are normal
    After comparing with https://github.com/nlohmann/json/blob/v3.9.0/test/src/fuzzer-parse_json.cpp I must agree data can be random so it should be accepted

--- tests/fuzz/BaseDecodeFuzz.cpp
+++ tests/fuzz/BaseDecodeFuzz.cpp
@@ -1,18 +1,15 @@
 #include <jwt-cpp/base.h>
-#include <openssl/evp.h>
-#include <vector>
 
 extern "C" {
 
-int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
-  std::vector<uint8_t> encoded(4*((Size+2)/3)+1, 0);
-
-  EVP_EncodeBlock(encoded.data(), Data, Size);
-  // if(EVP_EncodeBlock(encoded.data(), Data, Size) != encoded.size())
-    //abort(); // Critical error using OpenSSL
-                               
-  jwt::base::decode<jwt::alphabet::base64>(std::string{encoded.begin(), encoded.end() - 1}); // TO DO: Compare against input
-  return 0; // Non-zero return values are reserved for future use.
+int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {  
+    try {
+        const auto bin = jwt::base::decode<jwt::alphabet::base64>(std::string{(char*)Data, Size});
+    }
+    catch (const std::runtime_error&) {
+        // parse errors are ok, because input may be random bytes
+    }
+    return 0; // Non-zero return values are reserved for future use.
 }
 
 }
