d09bcd9801822ce2990d4a05a0826d4384316bff
Chris Mc <prince.chrismc@gmail.com>
2020-07-28 23:07:43 -0400

    decoding twice should produce the same result
    again based on https://github.com/nlohmann/json/blob/v3.9.0/test/src/fuzzer-parse_json.cpp

--- tests/fuzz/TokenDecodeFuzz.cpp
+++ tests/fuzz/TokenDecodeFuzz.cpp
@@ -3,7 +3,34 @@
 extern "C" {
 
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
-  jwt::decode(std::string{(char *)Data, Size});
+  
+  
+    try
+    {
+        // step 1: parse input
+        const auto jwt1 = jwt::decode(std::string{(char *)Data, Size});
+
+        try
+        {
+            // step 2: round trip
+            std::string s1 = jwt1.get_token();
+            const auto jwt2 = jwt::decode(s1);
+
+            // claims must match
+            assert(jwt1.get_header_claims() == jwt2.get_header_claims());
+            assert(jwt1.get_payload_claims() == jwt2.get_payload_claims());
+        }
+        catch (...)
+        {
+            // parsing raw data twice must not fail
+            assert(false);
+        }
+    }
+    catch (...)
+    {
+        // parse errors are ok, because input may be random bytes
+    }
+  
   return 0; // Non-zero return values are reserved for future use.
 }
 }
