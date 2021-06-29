1ce372f8194a1f8458536d27f86a76b3efa833f5
Tarun Chinmai Sekar <tchinmai7@gmail.com>
2021-06-29 02:46:03 -0700

    Clang-format

--- include/jwt-cpp/jwt.h
+++ include/jwt-cpp/jwt.h
@@ -1800,8 +1800,8 @@ namespace jwt {
 		};
 
 		template<typename object_type, typename string_type>
-		using is_count_signature = typename std::is_integral<decltype(std::declval<const object_type>().count(
-			std::declval<const string_type>()))>;
+		using is_count_signature = typename std::is_integral<decltype(
+			std::declval<const object_type>().count(std::declval<const string_type>()))>;
 
 		template<typename object_type, typename value_type, typename string_type>
 		using is_subcription_operator_signature =
