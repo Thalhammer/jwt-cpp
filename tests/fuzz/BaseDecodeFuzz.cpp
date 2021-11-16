e9952bf454bbd887acd9a91e43d324d6de25b7d5
prince-chrismc <prince-chrismc@users.noreply.github.com>
2021-11-16 07:03:06 +0000

    Apply automatic changes

--- tests/fuzz/BaseDecodeFuzz.cpp
+++ tests/fuzz/BaseDecodeFuzz.cpp
@@ -2,17 +2,12 @@
 
 extern "C" {
 
-int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size)
-{
-	try
-	{
+int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size) {
+	try {
 		const auto bin = jwt::base::decode<jwt::alphabet::base64>(std::string{(char*)Data, Size});
-	}
-    catch (const std::runtime_error&)
-	{
+	} catch (const std::runtime_error&) {
 		// parse errors are ok, because input may be random bytes
 	}
 	return 0; // Non-zero return values are reserved for future use.
 }
-
 }
