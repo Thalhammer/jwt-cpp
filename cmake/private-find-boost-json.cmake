3bcace6a9dc442d4d33aa4de52e2ee4b9a92d38d
Chris Mc <prince.chrismc@gmail.com>
2021-11-21 20:39:29 -0800

    Update private-find-boost-json.cmake

--- cmake/private-find-boost-json.cmake
+++ cmake/private-find-boost-json.cmake
@@ -5,7 +5,8 @@ endif()
 unset(BOOSTJSON_INCLUDE_DIR CACHE)
 find_path(BOOSTJSON_INCLUDE_DIR "boost/json.hpp" "boost/json/src.hpp")
 if(EXISTS "${BOOSTJSON_INCLUDE_DIR}/boost/json.hpp")
-  configure_file(private-boost-json.cpp.in private-boost-json.cpp COPYONLY)
+  file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/private-boost-json.cpp.in" "#include <boost/json/src.hpp>")
+  configure_file("${CMAKE_CURRENT_BINARY_DIR}/private-boost-json.cpp.in" private-boost-json.cpp COPYONLY)
   add_library(boost_json "${BOOSTJSON_INCLUDE_DIR}/boost/json.hpp"
                          "${BOOSTJSON_INCLUDE_DIR}/boost/json/src.hpp"
                          "${CMAKE_CURRENT_BINARY_DIR}/private-boost-json.cpp")
