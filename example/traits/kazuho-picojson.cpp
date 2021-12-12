df2849bfd7affa8efa727d5f6791d83056c68ca7
Chris McArthur <prince.chrismc@gmail.com>
2021-12-12 11:38:11 -0800

    fix example

--- example/traits/kazuho-picojson.cpp
+++ example/traits/kazuho-picojson.cpp
@@ -3,19 +3,21 @@
 #include <iostream>
 #include <sstream>
 
-using sec = std::chrono::seconds;
-using min = std::chrono::minutes;
-
 int main() {
-	jwt::claim from_raw_json;
+	using sec = std::chrono::seconds;
+	using min = std::chrono::minutes;
+	using traits = jwt::traits::kazuho_picojson;
+	using claim = jwt::basic_claim<traits>;
+
+	claim from_raw_json;
 	std::istringstream iss{R"##({"api":{"array":[1,2,3],"null":null}})##"};
 	iss >> from_raw_json;
 
-	jwt::claim::set_t list{"once", "twice"};
+	claim::set_t list{"once", "twice"};
 	std::vector<int64_t> big_numbers{727663072ULL, 770979831ULL, 427239169ULL, 525936436ULL};
 
 	const auto time = jwt::date::clock::now();
-	const auto token = jwt::create()
+	const auto token = jwt::create<traits>()
 						   .set_type("JWT")
 						   .set_issuer("auth.mydomain.io")
 						   .set_audience("mydomain.io")
@@ -25,8 +27,8 @@ int main() {
 						   .set_payload_claim("boolean", picojson::value(true))
 						   .set_payload_claim("integer", picojson::value(int64_t{12345}))
 						   .set_payload_claim("precision", picojson::value(12.345))
-						   .set_payload_claim("strings", jwt::claim(list))
-						   .set_payload_claim("array", jwt::claim(big_numbers.begin(), big_numbers.end()))
+						   .set_payload_claim("strings", claim(list))
+						   .set_payload_claim("array", claim(big_numbers.begin(), big_numbers.end()))
 						   .set_payload_claim("object", from_raw_json)
 						   .sign(jwt::algorithm::none{});
 
@@ -35,7 +37,7 @@ int main() {
 	const auto api_array = decoded.get_payload_claims()["object"].to_json().get("api").get("array");
 	std::cout << "api array = " << api_array << std::endl;
 
-	jwt::verify()
+	jwt::verify<traits>()
 		.allow_algorithm(jwt::algorithm::none{})
 		.with_issuer("auth.mydomain.io")
 		.with_audience("mydomain.io")
