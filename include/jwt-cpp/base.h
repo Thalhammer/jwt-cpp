d95969be2d3e6b987e836c085810b171226682ab
Chris McArthur <prince.chrismc@gmail.com>
2022-03-12 21:53:58 -0800

    refact `jwt::base` to a namespace
    instead of a class

--- include/jwt-cpp/base.h
+++ include/jwt-cpp/base.h
@@ -69,147 +69,148 @@ namespace jwt {
 	} // namespace alphabet
 
 	/**
-	 * \brief Alphabet generic methods for working with encoding/decoding the base64 family
+	 * \brief A collection of fexlible functions for working with base64 and base64url
 	 */
-	class base {
-	public:
-		template<typename T>
-		static std::string encode(const std::string& bin) {
-			return encode(bin, T::data(), T::fill());
-		}
-		template<typename T>
-		static std::string decode(const std::string& base) {
-			return decode(base, T::data(), T::fill());
-		}
-		template<typename T>
-		static std::string pad(const std::string& base) {
-			return pad(base, T::fill());
-		}
-		template<typename T>
-		static std::string trim(const std::string& base) {
-			return trim(base, T::fill());
-		}
+	namespace base {
 
-	private:
-		static std::string encode(const std::string& bin, const alphabet::soup& alphabet, const std::string& fill) {
-			size_t size = bin.size();
-			std::string res;
+		namespace details {
+			inline std::string encode(const std::string& bin, const alphabet::soup& alphabet, const std::string& fill) {
+				size_t size = bin.size();
+				std::string res;
 
-			// clear incomplete bytes
-			size_t fast_size = size - size % 3;
-			for (size_t i = 0; i < fast_size;) {
-				uint32_t octet_a = static_cast<unsigned char>(bin[i++]);
-				uint32_t octet_b = static_cast<unsigned char>(bin[i++]);
-				uint32_t octet_c = static_cast<unsigned char>(bin[i++]);
+				// clear incomplete bytes
+				size_t fast_size = size - size % 3;
+				for (size_t i = 0; i < fast_size;) {
+					uint32_t octet_a = static_cast<unsigned char>(bin[i++]);
+					uint32_t octet_b = static_cast<unsigned char>(bin[i++]);
+					uint32_t octet_c = static_cast<unsigned char>(bin[i++]);
+
+					uint32_t triple = (octet_a << 0x10) + (octet_b << 0x08) + octet_c;
+
+					res += alphabet[(triple >> 3 * 6) & 0x3F];
+					res += alphabet[(triple >> 2 * 6) & 0x3F];
+					res += alphabet[(triple >> 1 * 6) & 0x3F];
+					res += alphabet[(triple >> 0 * 6) & 0x3F];
+				}
+
+				if (fast_size == size) return res;
+
+				size_t mod = size % 3;
+
+				uint32_t octet_a = fast_size < size ? static_cast<unsigned char>(bin[fast_size++]) : 0;
+				uint32_t octet_b = fast_size < size ? static_cast<unsigned char>(bin[fast_size++]) : 0;
+				uint32_t octet_c = fast_size < size ? static_cast<unsigned char>(bin[fast_size++]) : 0;
 
 				uint32_t triple = (octet_a << 0x10) + (octet_b << 0x08) + octet_c;
 
-				res += alphabet[(triple >> 3 * 6) & 0x3F];
-				res += alphabet[(triple >> 2 * 6) & 0x3F];
-				res += alphabet[(triple >> 1 * 6) & 0x3F];
-				res += alphabet[(triple >> 0 * 6) & 0x3F];
-			}
-
-			if (fast_size == size) return res;
-
-			size_t mod = size % 3;
-
-			uint32_t octet_a = fast_size < size ? static_cast<unsigned char>(bin[fast_size++]) : 0;
-			uint32_t octet_b = fast_size < size ? static_cast<unsigned char>(bin[fast_size++]) : 0;
-			uint32_t octet_c = fast_size < size ? static_cast<unsigned char>(bin[fast_size++]) : 0;
-
-			uint32_t triple = (octet_a << 0x10) + (octet_b << 0x08) + octet_c;
-
-			switch (mod) {
-			case 1:
-				res += alphabet[(triple >> 3 * 6) & 0x3F];
-				res += alphabet[(triple >> 2 * 6) & 0x3F];
-				res += fill;
-				res += fill;
-				break;
-			case 2:
-				res += alphabet[(triple >> 3 * 6) & 0x3F];
-				res += alphabet[(triple >> 2 * 6) & 0x3F];
-				res += alphabet[(triple >> 1 * 6) & 0x3F];
-				res += fill;
-				break;
-			default: break;
-			}
-
-			return res;
-		}
-
-		static std::string decode(const std::string& base, const alphabet::soup& alphabet, const std::string& fill) {
-			size_t size = base.size();
-
-			size_t fill_cnt = 0;
-			while (size > fill.size()) {
-				if (base.substr(size - fill.size(), fill.size()) == fill) {
-					fill_cnt++;
-					size -= fill.size();
-					if (fill_cnt > 2) throw std::runtime_error("Invalid input: too much fill");
-				} else
+				switch (mod) {
+				case 1:
+					res += alphabet[(triple >> 3 * 6) & 0x3F];
+					res += alphabet[(triple >> 2 * 6) & 0x3F];
+					res += fill;
+					res += fill;
 					break;
+				case 2:
+					res += alphabet[(triple >> 3 * 6) & 0x3F];
+					res += alphabet[(triple >> 2 * 6) & 0x3F];
+					res += alphabet[(triple >> 1 * 6) & 0x3F];
+					res += fill;
+					break;
+				default: break;
+				}
+
+				return res;
 			}
 
-			if ((size + fill_cnt) % 4 != 0) throw std::runtime_error("Invalid input: incorrect total size");
+			inline std::string decode(const std::string& base, const alphabet::soup& alphabet,
+									  const std::string& fill) {
+				size_t size = base.size();
 
-			size_t out_size = size / 4 * 3;
-			std::string res;
-			res.reserve(out_size);
+				size_t fill_cnt = 0;
+				while (size > fill.size()) {
+					if (base.substr(size - fill.size(), fill.size()) == fill) {
+						fill_cnt++;
+						size -= fill.size();
+						if (fill_cnt > 2) throw std::runtime_error("Invalid input: too much fill");
+					} else
+						break;
+				}
 
-			auto get_sextet = [&](size_t offset) {
-				return alphabet::index(alphabet, base[offset]);
-			};
+				if ((size + fill_cnt) % 4 != 0) throw std::runtime_error("Invalid input: incorrect total size");
 
-			size_t fast_size = size - size % 4;
-			for (size_t i = 0; i < fast_size;) {
-				uint32_t sextet_a = get_sextet(i++);
-				uint32_t sextet_b = get_sextet(i++);
-				uint32_t sextet_c = get_sextet(i++);
-				uint32_t sextet_d = get_sextet(i++);
+				size_t out_size = size / 4 * 3;
+				std::string res;
+				res.reserve(out_size);
 
-				uint32_t triple = (sextet_a << 3 * 6) + (sextet_b << 2 * 6) + (sextet_c << 1 * 6) + (sextet_d << 0 * 6);
+				auto get_sextet = [&](size_t offset) { return alphabet::index(alphabet, base[offset]); };
 
-				res += static_cast<char>((triple >> 2 * 8) & 0xFFU);
-				res += static_cast<char>((triple >> 1 * 8) & 0xFFU);
-				res += static_cast<char>((triple >> 0 * 8) & 0xFFU);
+				size_t fast_size = size - size % 4;
+				for (size_t i = 0; i < fast_size;) {
+					uint32_t sextet_a = get_sextet(i++);
+					uint32_t sextet_b = get_sextet(i++);
+					uint32_t sextet_c = get_sextet(i++);
+					uint32_t sextet_d = get_sextet(i++);
+
+					uint32_t triple =
+						(sextet_a << 3 * 6) + (sextet_b << 2 * 6) + (sextet_c << 1 * 6) + (sextet_d << 0 * 6);
+
+					res += static_cast<char>((triple >> 2 * 8) & 0xFFU);
+					res += static_cast<char>((triple >> 1 * 8) & 0xFFU);
+					res += static_cast<char>((triple >> 0 * 8) & 0xFFU);
+				}
+
+				if (fill_cnt == 0) return res;
+
+				uint32_t triple = (get_sextet(fast_size) << 3 * 6) + (get_sextet(fast_size + 1) << 2 * 6);
+
+				switch (fill_cnt) {
+				case 1:
+					triple |= (get_sextet(fast_size + 2) << 1 * 6);
+					res += static_cast<char>((triple >> 2 * 8) & 0xFFU);
+					res += static_cast<char>((triple >> 1 * 8) & 0xFFU);
+					break;
+				case 2: res += static_cast<char>((triple >> 2 * 8) & 0xFFU); break;
+				default: break;
+				}
+
+				return res;
 			}
 
-			if (fill_cnt == 0) return res;
+			inline std::string pad(const std::string& base, const std::string& fill) {
+				std::string padding;
+				switch (base.size() % 4) {
+				case 1: padding += fill; JWT_FALLTHROUGH;
+				case 2: padding += fill; JWT_FALLTHROUGH;
+				case 3: padding += fill; JWT_FALLTHROUGH;
+				default: break;
+				}
 
-			uint32_t triple = (get_sextet(fast_size) << 3 * 6) + (get_sextet(fast_size + 1) << 2 * 6);
-
-			switch (fill_cnt) {
-			case 1:
-				triple |= (get_sextet(fast_size + 2) << 1 * 6);
-				res += static_cast<char>((triple >> 2 * 8) & 0xFFU);
-				res += static_cast<char>((triple >> 1 * 8) & 0xFFU);
-				break;
-			case 2: res += static_cast<char>((triple >> 2 * 8) & 0xFFU); break;
-			default: break;
+				return base + padding;
 			}
 
-			return res;
+			inline std::string trim(const std::string& base, const std::string& fill) {
+				auto pos = base.find(fill);
+				return base.substr(0, pos);
+			}
+		} // namespace details
+
+		template<typename T>
+		std::string encode(const std::string& bin) {
+			return details::encode(bin, T::data(), T::fill());
 		}
-
-		static std::string pad(const std::string& base, const std::string& fill) {
-			std::string padding;
-			switch (base.size() % 4) {
-			case 1: padding += fill; JWT_FALLTHROUGH;
-			case 2: padding += fill; JWT_FALLTHROUGH;
-			case 3: padding += fill; JWT_FALLTHROUGH;
-			default: break;
-			}
-
-			return base + padding;
+		template<typename T>
+		std::string decode(const std::string& base) {
+			return details::decode(base, T::data(), T::fill());
 		}
-
-		static std::string trim(const std::string& base, const std::string& fill) {
-			auto pos = base.find(fill);
-			return base.substr(0, pos);
+		template<typename T>
+		std::string pad(const std::string& base) {
+			return details::pad(base, T::fill());
 		}
-	};
+		template<typename T>
+		std::string trim(const std::string& base) {
+			return details::trim(base, T::fill());
+		}
+	} // namespace base
 } // namespace jwt
 
 #endif
