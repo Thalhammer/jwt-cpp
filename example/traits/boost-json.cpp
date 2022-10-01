2ff2dab70a9eeb2acfb170d0ce2494a6407fe3fa
Chris Mc <prince.chrismc@gmail.com>
2022-10-01 21:57:45 +0200

    tidy

--- example/traits/boost-json.cpp
+++ example/traits/boost-json.cpp
@@ -34,9 +34,8 @@ int main() {
 						   .sign(jwt::algorithm::none{});
 	const auto decoded = jwt::decode<traits>(token);
 
-	for (auto& e : decoded.get_header_claims()) {
+	for (auto& e : decoded.get_header_claims())
 		std::cout << e.key() << " = " << e.value() << std::endl;
-	}
 
 	const auto array =
 		traits::as_array(decoded.get_payload_claim("object").to_json().as_object()["api"].as_object()["array"]);
