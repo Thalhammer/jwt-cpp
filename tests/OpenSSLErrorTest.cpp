827c5fb209652a58b092560c3e2b2a979aac68a8
Christopher McArthur <prince.chrismc@gmail.com>
2023-12-10 20:11:13 -0500

    fix copy paste

--- tests/OpenSSLErrorTest.cpp
+++ tests/OpenSSLErrorTest.cpp
@@ -937,7 +937,8 @@ TEST(OpenSSLErrorTest, EdDSACertificate) {
 }
 
 TEST(OpenSSLErrorTest, Ed25519Reference) {
-	ASSERT_THROW(jwt::algorithm::ed25519 alg{"", ""}, jwt::error:ecdsa_exception);
+	// No keys should throw
+	ASSERT_THROW(jwt::algorithm::ed25519("", ""), jwt::error:ecdsa_exception);
 
 	jwt::algorithm::ed25519 alg{ed25519_pub_key, ed25519_priv_key};
 	std::error_code ec;
