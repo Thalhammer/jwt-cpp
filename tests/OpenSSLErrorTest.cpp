4976de9ac5afd53d2af3eb67ec4453b2753ebb53
Christopher McArthur <prince.chrismc@gmail.com>
2023-12-26 09:11:48 -0500

    linter

--- tests/OpenSSLErrorTest.cpp
+++ tests/OpenSSLErrorTest.cpp
@@ -487,7 +487,7 @@ int EVP_PKEY_fromdata(EVP_PKEY_CTX* ctx, EVP_PKEY** ppkey, int selection, OSSL_P
 		return origMethod(ctx, ppkey, selection, params);
 }
 #else
-int PEM_write_bio_RSA_PUBKEY(BIO *bp, RSA *x) {
+int PEM_write_bio_RSA_PUBKEY(BIO* bp, RSA* x) {
 	static int (*origMethod)(BIO * bp, OPENSSL_CONST EVP_PKEY * x) = nullptr;
 	if (origMethod == nullptr) origMethod = (decltype(origMethod))dlsym(RTLD_NEXT, "PEM_write_bio_RSA_PUBKEY");
 	bool fail = fail_PEM_write_bio_RSA_PUBKEY & 1;
@@ -511,7 +511,7 @@ int RSA_set0_key(RSA* r, BIGNUM* n, BIGNUM* e, BIGNUM* d) {
 #endif
 
 long BIO_get_mem_data(BIO* b, char** pp) {
-	static int (*origMethod)(BIO* b, char** pp) = nullptr;
+	static int (*origMethod)(BIO * b, char** pp) = nullptr;
 	if (origMethod == nullptr) origMethod = (decltype(origMethod))dlsym(RTLD_NEXT, "BIO_get_mem_data");
 	bool fail = fail_BIO_get_mem_data & 1;
 	fail_BIO_get_mem_data = fail_BIO_get_mem_data >> 1;
