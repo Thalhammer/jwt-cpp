10febfe60bcbb25b45ec07c09e73b8aa0345ac64
Christopher McArthur <prince.chrismc@gmail.com>
2023-12-29 10:37:13 -0500

    linter

--- example/print-claims.cpp
+++ example/print-claims.cpp
@@ -3,8 +3,8 @@
 #include <jwt-cpp/jwt.h>
 
 int main() {
-	std::string token =
-		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJpc3MiOiJhdXRoMCIsInNhbXBsZSI6InRlc3QifQ.lQm3N2bVlqt2-1L-FsOjtR6uE-L4E9zJutMWKIe1v1M";
+	std::string token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJpc3MiOiJhdXRoMCIsInNhbXBsZSI6InRlc3QifQ.lQm3N2bVlqt2-"
+						"1L-FsOjtR6uE-L4E9zJutMWKIe1v1M";
 	auto decoded = jwt::decode(token);
 
 	for (auto& e : decoded.get_payload_json())
