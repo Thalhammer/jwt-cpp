622a254e27d8f723fa9064db6cd4e7f919df54f9
Christopher McArthur <prince.chrismc@gmail.com>
2023-12-31 22:19:55 -0500

    format changes

--- include/jwt-cpp/traits/kazuho-picojson/defaults.h
+++ include/jwt-cpp/traits/kazuho-picojson/defaults.h
@@ -1,6 +1,10 @@
 #ifndef JWT_CPP_KAZUHO_PICOJSON_DEFAULTS_H
 #define JWT_CPP_KAZUHO_PICOJSON_DEFAULTS_H
 
+#ifndef JWT_DISABLE_PICOJSON
+/* #undef JWT_DISABLE_PICOJSON */
+#endif
+
 #include "traits.h"
 
 namespace jwt {
