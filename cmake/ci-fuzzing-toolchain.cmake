3b15555df1ae276760cc0c0c5599fec48e674459
Chris Mc <prince.chrismc@gmail.com>
2024-01-09 15:49:04 -0500

    more global fuzzing flags to toolchain file

---
+++ cmake/ci-fuzzing-toolchain.cmake
@@ -0,0 +1,7 @@
+include("linux-clang-toolchain.cmake")
+
+set(CMAKE_BUILD_TYPE "Debug")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g -O1 -fsanitize=fuzzer,address,signed-integer-overflow,undefined -fno-omit-frame-pointer")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -O1 -fsanitize=fuzzer,address,signed-integer-overflow,undefined -fno-omit-frame-pointer")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -g -O1 -fsanitize=fuzzer,address,signed-integer-overflow,undefined -fno-omit-frame-pointer")
+set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -g -O1 -fsanitize=fuzzer,address,signed-integer-overflow,undefined -fno-omit-frame-pointer")
---
+++ cmake/linux-clang-toolchain.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_SYSTEM_NAME Linux)
+
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++)
