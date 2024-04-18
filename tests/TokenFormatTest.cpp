73a2e3c34224145fb3c1466b3f839b73318fbcc4
Chris Mc <prince.chrismc@gmail.com>
2024-04-17 18:47:47 -0700

    Update TokenFormatTest.cpp

--- tests/TokenFormatTest.cpp
+++ tests/TokenFormatTest.cpp
@@ -18,7 +18,7 @@ TEST(TokenFormatTest, InvalidJSON) {
 #include "jwt-cpp/traits/nlohmann-json/traits.h"
 
 TEST(TokenFormatTEst, Issue343) {
-	ASSERT_NOTHROW(
-std::string token = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjYXV0aDAiLCJleHAiOjE3MTMzODgxNjgsInN1YiI6InRlc3RfdXNlciJ9.dlAk0mSWk1Clzfi1PMq7Omxun3EyEqh-AAu-fTkpabA67ZKenawAQhZO8glY93flukpJCqHLVtukaes6ZSOjGw";
-jwt::decoded_jwt<jwt::traits::nlohmann_json> decoded(token);
+	std::string token = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjYXV0aDAiLCJleHAiOjE3MTMzODgxNjgsInN1YiI6InRlc3RfdXNlciJ9.dlAk0mSWk1Clzfi1PMq7Omxun3EyEqh-AAu-fTkpabA67ZKenawAQhZO8glY93flukpJCqHLVtukaes6ZSOjGw";
+	auto decoded = jwt::decoded_jwt<jwt::traits::nlohmann_json> decoded(token);
+	ASSERT_TRUE(decoded.has_algorithm());
 }
