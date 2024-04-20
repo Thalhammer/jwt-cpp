e9cd684d027eb40396f2db6b07f41275415806a4
Chris Mc <prince.chrismc@gmail.com>
2024-04-20 13:34:09 -0700

    Add token format test from #341 (#343)
    * Update TokenFormatTest.cpp
    
    * Update TokenFormatTest.cpp
    
    * Update TokenFormatTest.cpp
    
    * cleanup test for issue
    
    * clang-format

--- tests/TokenFormatTest.cpp
+++ tests/TokenFormatTest.cpp
@@ -14,3 +14,22 @@ TEST(TokenFormatTest, InvalidChar) {
 TEST(TokenFormatTest, InvalidJSON) {
 	ASSERT_THROW(jwt::decode("YXsiYWxnIjoibm9uZSIsInR5cCI6IkpXUyJ9YQ.eyJpc3MiOiJhdXRoMCJ9."), std::runtime_error);
 }
+
+#include "jwt-cpp/traits/nlohmann-json/traits.h"
+
+TEST(TokenFormatTest, GitHubIssue341) {
+	std::string token =
+		"eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjYXV0aDAiLCJleHAiOjE3MTMzODgxNjgsInN1YiI6InRlc3RfdXNlciJ9."
+		"dlAk0mSWk1Clzfi1PMq7Omxun3EyEqh-AAu-fTkpabA67ZKenawAQhZO8glY93flukpJCqHLVtukaes6ZSOjGw";
+	auto decoded = jwt::decoded_jwt<jwt::traits::nlohmann_json>(token);
+
+	ASSERT_TRUE(decoded.has_algorithm());
+	ASSERT_TRUE(decoded.has_type());
+	ASSERT_TRUE(decoded.has_issuer());
+	ASSERT_TRUE(decoded.has_subject());
+
+	ASSERT_EQ("ES256", decoded.get_algorithm());
+	ASSERT_EQ("JWT", decoded.get_type());
+	ASSERT_EQ("cauth0", decoded.get_issuer());
+	ASSERT_EQ("test_user", decoded.get_subject());
+}
