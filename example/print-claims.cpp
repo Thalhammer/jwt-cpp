f6b671572893e24d6ff2ef6d6f865abf449b5f84
Chris Mc <prince.chrismc@gmail.com>
2025-05-13 12:14:50 -0400

    formatting

--- example/print-claims.cpp
+++ example/print-claims.cpp
@@ -3,7 +3,8 @@
 #include <jwt-cpp/jwt.h>
 
 int main() {
-	std::string const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJpc3MiOiJhdXRoMCIsInNhbXBsZSI6InRlc3QifQ.lQm3N2bVlqt2-"
+	const std::string token = 
+						"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJpc3MiOiJhdXRoMCIsInNhbXBsZSI6InRlc3QifQ.lQm3N2bVlqt2-"
 						"1L-FsOjtR6uE-L4E9zJutMWKIe1v1M";
 	auto decoded = jwt::decode(token);
 
